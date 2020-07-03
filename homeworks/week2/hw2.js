c021144253b1afdd788a9f5bea05e4d755954b52
AlvinTsai <goape7914@gmail.com>
2020-07-04 01:41:25 +0800

    week2HwDone

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,18 @@
-function capitalize(str) {
-  
+
+function capitalize(n){
+    var change = n;
+    var changeCode = change.charCodeAt(0);
+    var changedWord = '';
+    //console.log(change[2])
+    for (var i=0; i<change.length; i++){
+        if(i==0 && change[0] >='a' && change[0]<='z'){
+            var changed = String.fromCharCode(changeCode-32);
+            changedWord += changed;
+            //console.log(changedWord);
+        }else{
+            changedWord += change[i];
+        }
+    }return changedWord;  
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize('zzzzzl1265489n'))
