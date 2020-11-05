c7a9a2ec0ff5544053b4aa806f12feed409e1877
AlvinTsai <goape7914@gmail.com>
2020-11-05 15:37:41 +0800

    week13_fixed_week14_done

--- homeworks/week13/hw2/src/templates.js
+++ homeworks/week13/hw2/src/templates.js
@@ -1,5 +1,4 @@
 export const cssTemplate = `
-  .card {margin-top:12px;}
   .card {
     margin-top: 16px;
   }
@@ -17,24 +16,6 @@ export const cssTemplate = `
   }
 `
 
-
-const formTemplate = `		
-  <div>
-    <form class='add-comment-form'>
-      <div class="form-group">
-        <label for="nickname">Nickname</label>
-        <input name='nickname' type="text" class="form-control" id="form-nickname">
-      </div>
-      <div class="form-group">
-        <label for="content-textarea">Comment</label>
-        <textarea name='content' class="form-control" id="content-textarea" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">送出</button>
-    </form>
-    <div class='comments'>
-    </div>
-  </div>
-`
 
 export function getForm(className, commentsClassName) {
   return`
